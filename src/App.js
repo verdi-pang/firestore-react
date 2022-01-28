d0a41be01a5c12aeb3ef95444ca58c6eb3bebed5
verdi-pang <75467831+verdi-pang@users.noreply.github.com>
2022-01-28 04:17:42 -0800

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from 'react';
+import { db } from './firebase.js'
+import { addDoc, collection, getDocs } from 'firebase/firestore'
+import { Button, Container, ListItem, ListItemText, TextField, Typography } from '@mui/material';
 
 function App() {
+  const [newTodo, setNewTodo] = useState('')
+
+  const [todos, setTodos] = useState([]);
+  const todoCollectionRef = collection(db, 'todos')
+
+  const createTodo = async () => {
+    await addDoc(todoCollectionRef, {todo: newTodo});
+  };
+
+  useEffect(()=>{
+    const getTodos = async() => {
+      const data = await getDocs(todoCollectionRef);
+      // console.log(data);
+      setTodos(data.docs.map((doc)=>({...doc.data(), id: doc.id})));
+    }
+
+    getTodos()
+  },[])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Container sx={{ boxShadow: 1, p:3, mt:2, borderRadius: 2, backgroundColor:'#FFF'}}
+      style={{display: 'flex', flexDirection: 'column'}}
+      maxWidth='sm'>
+        <Typography variant='h4'>
+          TODO LIST
+        </Typography>
+        <TextField label='Add Todo' onChange={(e)=> {setNewTodo(e.target.value)}} sx={{mt:1}}/>
+        <Button onClick={createTodo} variant='contained' sx={{mt:1}}>Create Todo</Button>
+        
+        {todos.map((todo)=>
+          {return <ListItem key={todo.id} sx={{mt:2, boxShadow: 3, borderRadius:2}}>
+            <ListItemText primary={todo.todo} />
+            
+          </ListItem>;
+        })}
+      </Container>
     </div>
   );
 }
---
+++ src/firebase.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+import { getFirestore } from '@firebase/firestore'
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyA_LgEnjSN1q9L9TlI8ZbVdZoZ0BqfYQm0",
+  authDomain: "fir-react-27e6b.firebaseapp.com",
+  projectId: "fir-react-27e6b",
+  storageBucket: "fir-react-27e6b.appspot.com",
+  messagingSenderId: "466834619243",
+  appId: "1:466834619243:web:c2070290fcaf71d86aaa2e"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export const db = getFirestore();
